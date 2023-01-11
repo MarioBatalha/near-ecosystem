140745f5e493c6363205108387dc449408c1b2ad
MarioBatalha <mariobatalha9@gmail.com>
2023-01-12 00:04:42 +0100

    added json format

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1f95ec732d4db911"; }
+/******/ 		__webpack_require__.h = function() { return "571f24dc1b5ffa8b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
